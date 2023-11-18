2bed816dd056e18bd35333598082ec1d309986b2
ARTHUR SILVA BERDUSCO DE SOUZA <arthur.sbsouza@senacsp.edu.br>
2023-11-17 21:50:25 -0300

    aula de poo cadastro de tarefas com spring

--- Programação Orientada a Objetos/10-11-23/epictask/src/main/java/br/senac/sp/epictask/model/Task.java
+++ Programação Orientada a Objetos/10-11-23/epictask/src/main/java/br/senac/sp/epictask/model/Task.java
@@ -4,8 +4,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
-
 
 //Builerplate - codigo repetitivo - contrutor getter setter / @Data
 // ORM (converte db em java e java em db)- JPA 
@@ -15,8 +17,18 @@ public class Task {
     
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
+
+    @NotBlank
     String title;
+
+    // not null = nao preencher
+    // not empty = "   "
+    // not blank = ""
+
+    @Size(min = 10, message="digite uma descrição com pelo menos 10 caracteres")
     String description;
+
+    @Min(value = 1, message="não pode ser negativo")
     Integer score;
 
 }
