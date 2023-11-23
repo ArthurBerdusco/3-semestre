2264fa294c129862b6e385ccbddc9d9b67bd0b6a
Arthur <arthur.berdusco1@gmail.com>
2023-11-23 01:34:16 -0300

    atualizado css

--- Programação Orientada a Objetos/Projeto Final/expense/src/main/java/br/senac/sp/expense/model/Expense.java
+++ Programação Orientada a Objetos/Projeto Final/expense/src/main/java/br/senac/sp/expense/model/Expense.java
@@ -9,6 +9,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Entity
@@ -18,11 +21,20 @@ public class Expense {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
 
+    @NotNull(message = "A data não pode ser nula")
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     Date date;
+
     String description;
+
+    @Pattern(regexp = "(?!^Selecione$).*", message = "Você deve selecionar uma categoria válida")
     String category;
+
+    @Pattern(regexp = "(?!^Selecione$).*", message = "Você deve selecionar o tipo de pagamento")
     String typePayment;
+
+    @NotNull(message = "O valor não pode ser nulo")
+    @Min(value = 0, message = "não pode ser negativo")
     BigDecimal amount;
 
 }
